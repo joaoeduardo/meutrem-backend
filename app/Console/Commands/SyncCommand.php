bebf36c7fe0954179d408d42adb39d8dd5c3a13b
João Eduardo <joaoeduardo@live.co.uk>
2017-08-24 18:27:47 +0000

    fix sync command bug

--- app/Console/Commands/SyncCommand.php
+++ app/Console/Commands/SyncCommand.php
@@ -2,12 +2,15 @@
 
 namespace App\Console\Commands;
 
+use App\Entities\Line;
 use App\Entities\Occurrence;
+use App\Entities\Status;
 use App\Repositories\LineRepository;
 use App\Repositories\StatusRepository;
 use Carbon\Carbon;
 use Doctrine\ORM\EntityManagerInterface;
 use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
 
 class SyncCommand extends Command
 {
@@ -69,43 +72,64 @@ class SyncCommand extends Command
     {
         $json = file_get_contents(env('CPTM_API'));
 
-        $data = json_decode($json, true);
+        $data = new Collection(json_decode($json, true));
 
-        foreach ($data as $item) {
-            $line = $this->lineRepository->find($item['LinhaId']);
-
-            $actualStatus = $this->statusRepository->firstOrCreate(['name' => $item['Status']]);
-
-            if ($line->getOccurrences()->count() == 0) {
-
-                $occurrence = new Occurrence();
-
-                $occurrence->setDescription($item['Descricao']);
-                $occurrence->setStatus($actualStatus);
-                $occurrence->setLine($line);
-                $occurrence->setStartedAt($item['DataGeracao']);
-
-                $this->manager->persist($occurrence);
-            } else {
-                if ($line->getOccurrences()->last()->getDescription() != $item['Descricao']) {
-                    $line->getOccurrences()->last()->setFinishedAt($item['DataGeracao']);
-
-                    $this->manager->persist($line);
-
-                    $occurrence = new Occurrence();
-
-                    $occurrence->setDescription($item['Descricao']);
-                    $occurrence->setStatus($actualStatus);
-                    $occurrence->setLine($line);
-                    $occurrence->setStartedAt($item['DataGeracao']);
-
-                    $this->manager->persist($occurrence);
-                }
-            }
-        }
+        $data->map(function ($item) {
+            $this->iterate($item);
+        });
 
         $this->manager->flush();
 
         $this->line(Carbon::now()->toDateTimeString());
+    }
+
+    public function iterate($item)
+    {
+        $line = $this->lineRepository->find($item['LinhaId']);
+
+        $actualStatus = $this->statusRepository->firstOrCreate(['name' => $item['Status']]);
+
+        if ($line->getOccurrences()->count() === 0) {
+            $this->startOccurrence($item['Descricao'], $actualStatus, $line, $item['DataGeracao']);
+
+            return;
+        }
+
+        $lastOccurrence = $line->getOccurrences()->last();
+
+        $lastStatus = $lastOccurrence->getStatus();
+
+        $statusChanged = $actualStatus->getName() !== $lastStatus->getName();
+
+        $descriptionChanged = $lastOccurrence->getDescription() !== $item['Descricao'];
+
+        $emptyOccurrences = is_null($lastOccurrence);
+
+        if ($statusChanged || $descriptionChanged) {
+            $this->finishOccurrence($lastOccurrence, $item['DataGeracao']);
+        }
+
+        if ($statusChanged || $descriptionChanged || $emptyOccurrences) {
+            $this->startOccurrence($item['Descricao'], $actualStatus, $line, $item['DataGeracao']);
+        }
+    }
+
+    public function finishOccurrence(Occurrence $occurrence, string $finishedAt)
+    {
+        $occurrence->setFinishedAt($finishedAt);
+
+        $this->manager->persist($occurrence);
+    }
+
+    public function startOccurrence(string $description, Status $status, Line $line, string $startedAt)
+    {
+        $occurrence = new Occurrence();
+
+        $occurrence->setDescription($description);
+        $occurrence->setStatus($status);
+        $occurrence->setLine($line);
+        $occurrence->setStartedAt($startedAt);
+
+        $this->manager->persist($occurrence);
     }
 }
